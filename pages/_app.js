5be1b7978409219e13523cf27137fbfe23ade16d
Finn Woelm <public@finnwoelm.com>
2020-07-16 07:26:09 +0200

    Add basic CSS styling
    Make the demo pages look a little bit nicer.

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+/*******************************************************************************
+ * NOTE: You do not need a custom _app.js file to use next-on-netlify. I just
+ *       wanted to add some CSS, so that the demo looks a little prettier :)
+ ******************************************************************************/
+
+import '../styles.css'
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
