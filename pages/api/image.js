5864c3f6419ec6dcde032faaba709c18fd3eb3be
Finn Woelm <public@finnwoelm.com>
2020-09-06 22:06:27 +0200

    /image API endpoint: Update image
    Replace JPEG with GIF as requested on Twitter :)

--- pages/api/image.js
+++ pages/api/image.js
@@ -1,8 +1,9 @@
-import rick from "buffer-loader!../../rick.jpg";
+// Image source: Sumatangwang via Tenor: https://tenor.com/bfLqf.gif
+import rick from "buffer-loader!../../rick.gif";
 
 export default (req, res) => {
   res.status(200);
-  res.setHeader("Content-Type", "image/jpeg");
+  res.setHeader("Content-Type", "image/gif");
   // Send the image buffer. There are many other ways to send images and other
   // files. For example, you can create a buffer from a base64-encoded string
   // of an image: https://stackoverflow.com/a/28440633/6451879
